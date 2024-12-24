561030cd50dfe4f9fb1464d6016faf9153ea342d
Abhishek Singh <abhiwebdev2718@gmail.com>
2024-12-24 21:05:54 +0530

    exporting google-signin

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { GoogleSigninService } from "@byldd/expo-google-signin";
+import { FacebookSigninService } from "@byldd/expo-facebook-signin";
+
+export {
+    GoogleSigninService,
+    FacebookSigninService
+}
